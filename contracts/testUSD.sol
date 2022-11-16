19f6fd09581f4c2633d35848f5dd01a707fa0632
Frost Corealis <89814258+FrostCorealis@users.noreply.github.com>
2022-11-16 08:52:06 -0500

    Update testUSD.sol

--- contracts/testUSD.sol
+++ contracts/testUSD.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: Chainlink Hackathon Fall 2022
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract testUSD is Ownable, ERC20 {
+
+    constructor() ERC20("Test USD", "testUSD") {
+        _mint(address(this), 1000000000000000000000000000000000000000);
+    }
+
+    function Produce(uint _amount) public {
+        _mint(msg.sender, _amount);
+    }     
+    //Ownerless for testing purposes
+    function bankTransfer(address to, uint256 amount) external returns (bool) {
+        _transfer(address(this), to, amount);
+        return true;
+    }
+
+}
