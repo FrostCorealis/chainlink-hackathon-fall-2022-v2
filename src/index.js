ab9d73356594eff85a5f8ad89076b5ec36fcb110
Frost Corealis <89814258+FrostCorealis@users.noreply.github.com>
2022-11-16 17:12:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Browse from './browse';
+import Create from './create';
+import Manage from './manage';
+import PolygonID from './polygonID';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+
+<div className="App-header">
+
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+
+    <Route path="/browse" element={<Browse />} />
+    <Route path="/create" element={<Create/>} />
+    <Route path="/manage" element={<Manage />} />
+    <Route path="/polygonID" element={<PolygonID />} />
+
+  </Routes>
+</BrowserRouter>
+
+
+</div>
+
+   
+  </React.StrictMode>
+);
+
 
