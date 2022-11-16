d3ba52aaeb34f012cc795945d1589a25b8312406
Frost Corealis <89814258+FrostCorealis@users.noreply.github.com>
2022-11-16 17:13:06 -0500

    Create polygonID.jsx

---
+++ src/polygonID.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import { render } from "react-dom";
+import { QRCode } from "react-qr-svg";
+import polygonLogo from './assets/polygonlogo.png';
+
+import { Link } from 'react-router-dom';  // managed links differently here because of the different react structure
+
+const styles = {
+  root: {
+    color: "#000000",
+    topMargin: "0",
+  },
+  zpkContainer: {
+    color: "#FFFFFF",
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    background: "#000000",
+    height: "150vh",
+    marginTop: "50px",
+  },
+  title: {
+    color: "#8247E5",
+    textStyle: "bold",
+    textShadow: "1px 1px 2px white, 5px 5px 25px #9ACD32, 3px 3px 5px #d9c7f7",
+    fontSize: "50px",
+  },
+  titleContainer: {
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  qrbox: {
+    background: "#FFFFFF",
+    margin: "35px",
+    width: "fit-content",
+    margin: "auto",
+  },
+  qrcontainer: {
+    maxWidth: "650px",
+    background: "#FFFFFF",
+    borderStyle: "solid",
+    borderWidth: "17px",
+    borderColor: "#8247E5",
+    margin: "auto",
+    marginBottom: "85px",
+    padding: "50px",
+    boxShadow: "3px 3px 29px #d9c7f7, 0px 0px 25px #9ACD32",
+  },
+
+};
+
+// update with your contract address
+const deployedContractAddress = "0x3A7772B5e1407524B6D57aaDAD226B4Ed56abFA7";
+
+// more info on query based requests: https://0xpolygonid.github.io/tutorials/wallet/proof-generation/types-of-auth-requests-and-proofs/#query-based-request
+const qrProofRequestJson = {
+  id: "f4c0d699-ddf6-44bd-807f-30ae81962039",
+  typ: "application/iden3comm-plain-json",
+  type: "https://iden3-communication.io/proofs/1.0/contract-invoke-request",
+  body: {
+    transaction_data: {
+      contract_address: "0x3A7772B5e1407524B6D57aaDAD226B4Ed56abFA7",
+      method_id: "b68967e2",
+      chain_id: 80001,
+      network: "polygon-mumbai"
+    },
+    reason: "chainlink hackathon stipend",
+    scope: [
+      {
+        id: 1,
+        circuit_id: "credentialAtomicQuerySig",
+        rules: {
+          query: {
+            allowed_issuers: ["*"],
+            req: {
+              hackathonparticipant: {
+                $eq: 1
+              }
+            },
+            schema: {
+              url:
+                "https://s3.eu-west-1.amazonaws.com/polygonid-schemas/de3875eb-a779-4cd6-8966-c75d1250c289.json-ld",
+              type: "ChainlinkHackathonFall2022Cactus"
+            }
+          }
+        }
+      }
+    ]
+  }
+};
+
+export default class App extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return (           
+
+      <div style={styles.zpkContainer}>
+        <div style={styles.titleContainer}>
+        <img src={polygonLogo} className="poly-logo" alt="logo" />
+        <h1 style={styles.title}>
+        {" "}Join the ZKP Stipend on CactuStipend{" "}
+        </h1>
+        <img src={polygonLogo} className="poly-logo" alt="logo" />
+        </div>
+  
+        <h1>
+          First, obtain{" "}
+          <a
+            href="https://platform-test.polygonid.com/claim-link/f4c0d699-ddf6-44bd-807f-30ae81962039"
+            target="_blank"
+          >
+         {" "}verification credentials{" "}
+          </a>from Polygon ID,</h1> 
+          <h1> and then scan this QR code to join the ZKP Stipend.
+        </h1>
+
+        <div style={styles.qrcontainer}>
+            <div style={styles.qrbox}>
+                <QRCode
+                    level="Q"
+                    style={{ width: 512 }}
+                    value={JSON.stringify(qrProofRequestJson)}
+                />
+            </div>
+        </div>
+        <h1>return to {" "}
+          <a href="https://learn-mumbai.frostc.repl.co/">
+            🌵 CactuStipend 🌵
+          </a></h1>
+       
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
