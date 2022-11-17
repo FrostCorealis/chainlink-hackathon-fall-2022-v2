205c554a18198ca1485db5c87310507794a67593
Frost Corealis <89814258+FrostCorealis@users.noreply.github.com>
2022-11-17 10:15:44 -0500

    Update polygonID.jsx

--- src/polygonID.jsx
+++ src/polygonID.jsx
@@ -21,7 +21,7 @@ const styles = {
   title: {
     color: "#8247E5",
     textStyle: "bold",
-    textShadow: "1px 1px 2px white, 5px 5px 25px #9ACD32, 3px 3px 5px #d9c7f7",
+    textShadow: "1px 1px 2px #9ACD32, 2px 2px 25px white, 3px 3px 5px #d9c7f7",
     fontSize: "35px",
   },
   titleContainer: {
