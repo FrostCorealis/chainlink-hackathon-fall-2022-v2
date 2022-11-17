fb79ca34b7615c5894f54a1bcb54621be300fb64
Frost Corealis <89814258+FrostCorealis@users.noreply.github.com>
2022-11-16 23:28:55 -0500

    Update browse.jsx

--- src/browse.jsx
+++ src/browse.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from "react-toastify";
 import { Slide, Zoom, Flip, Bounce } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import 'tippy.js/themes/light-border.css';
-import 'onfido-sdk-ui/dist/style.css';
 import { ethers } from 'ethers';
 import contractAbi from './utils/contractAbi.json';
 import ZKPcontractAbi from './utils/ZKPcontractAbi.json';
