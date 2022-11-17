34a6501a5a9574da12acbc616da32a30ea34269e
Frost Corealis <89814258+FrostCorealis@users.noreply.github.com>
2022-11-17 17:07:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -242,12 +242,12 @@ const CONTRACT_ADDRESS = "0xB576a883179d6A9018233e5833D9db8F726A764b";
 
         <img src={chainlinkLogo} className="App-logo" alt="logo" />
         <p>
-         Powered by Chainlink & Truflation.
+         Powered by Chainlink & Truflation
         </p>
         <img src={chainlinkLogo} className="App-logo" alt="logo" />
         </div>
         <div className="bottom2">
-          <p> Built on Polygon by Cactoid & Frost.</p>
+          <p> Built on Polygon by Team Cactus</p>
           <h5>Chainlink Hackathon Fall 2022</h5>
         </div>
 
